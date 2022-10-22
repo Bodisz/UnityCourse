730e1ca66e314bb26cb7cd68feba302bad89ef22
Bodisz <program@bodisz.hu>
2022-10-22 16:56:33 +0200

    Nap vége :)

--- Assets/Physics3D/ClickExplosion.cs
+++ Assets/Physics3D/ClickExplosion.cs
@@ -1,8 +1,19 @@
+using System;
 using UnityEngine;
 
 public class ClickExplosion : MonoBehaviour
 {
+    [SerializeField] float range;
+    [SerializeField] float maxExplosionForce;
+    [SerializeField] ParticleSystem particleSys;
+ 
+    Rigidbody[] rigidbodies;
     Vector3 lastHit;
+
+    private void Start()
+    {
+        rigidbodies = FindObjectsOfType<Rigidbody>();
+    }
     void Update()
     {
         Camera cam = Camera.main;
@@ -13,10 +24,40 @@ public class ClickExplosion : MonoBehaviour
         {
             Debug.Log($"HIT: {hit.collider.name} {hit.point}");
             lastHit = hit.point;
+
+            if (Input.GetMouseButton(0))
+            {
+                Explosion(hit.point);
+            }
         }
     }
 
-    private void OnDrawGizmos()
+    void Explosion(Vector3 point)
+    {
+        transform.position = point;
+        particleSys.Play();
+
+        for (int i = 0; i < rigidbodies.Length; i++)
+        {
+            Rigidbody rb = rigidbodies[i];
+            ExplosineOne(rb, point);
+        }
+    }
+    void ExplosineOne(Rigidbody rg, Vector3 p)
+    {
+        Vector3 direction = rg.transform.position - p;
+        float distance = direction.magnitude;
+
+        if (distance >= range) return;
+
+        direction /= distance;
+        float forceMultiplier = 1 - (distance / range);
+
+        Vector3 force = forceMultiplier * maxExplosionForce * direction;
+        rg.AddForce(force, ForceMode.Impulse);
+    }
+
+   void OnDrawGizmos()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawSphere(lastHit, 0.3f);
